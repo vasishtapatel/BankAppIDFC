369d29f229bf48b41773b90e583cf390346f543e
vasishtapatel <55794905+vasishtapatel@users.noreply.github.com>
2022-11-19 14:27:57 +0530

    Add files via upload

---
+++ banking_project/src/main/java/com/example/demo/model/Balance.java
@@ -0,0 +1,16 @@
+package com.example.demo.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.Data;
+@Data
+@Document(collection="balance")
+public class Balance {
+@Id
+private int custid;
+private String accNo;
+private String custName;
+private String city;
+private int balance;
+private String ifscCode;
+}
